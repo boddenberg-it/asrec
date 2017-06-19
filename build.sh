e4e73c558b333fa9c0c306d52f9109160601bd2c
André Boddenberg <andre@boddenberg.it>
2017-06-19 12:35:36 +0200

    Add simple start up test to build.sh for Jenkins

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -ex
+
 echo "[INFO] Compiling asrec.groovy to classes/"
 groovyc -d classes asrec.groovy
 echo "[INFO] Packaging asrec.jar"
 jar cvfm asrec.jar manifest -C classes/ .
-echo "[INFO] asrec build succesfully"
+echo "[INFO] Asrec build succesfully"
+
+if [ "$1" = "test" ]; then
+	echo "[INFO] Starting asrec.jar"
+	java -jar asrec.jar &
+	pid="$!"; sleep 5
+	kill "$pid"
+fi
